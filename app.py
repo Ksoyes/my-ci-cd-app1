2b5e5a55244509029de1e82a38f3e9502d9988b0
Росина Екатерина Алексеевна <s22270022@unn.ru>
2025-09-18 10:16:30 +0300

    tests

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/') #создает дополнительный маршрут, если запрос проходит успешно
+def hello():
+    return "Hi!"
+
+@app.route('/health')
+def health():
+    return "ok", 200
+
+if__name__='__main__':
+    app.run(host='0.0.0.0', port=5000) #на этом порту будет работать это приложение (5000)
